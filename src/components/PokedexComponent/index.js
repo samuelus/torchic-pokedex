4027939270331e2d2f1735ef0898a9438e3c657f
Adenka <13adena@gmail.com>
2019-12-31 10:46:43 +0100

    Styled scrollbar, styled Pokedex title

--- src/components/PokedexComponent/index.js
+++ src/components/PokedexComponent/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import List1 from '../../assets/pokedex.json';
 import List2 from '../../assets/pokemon.json';
 import Pokemon from '../PokemonComponent';
-import {Paper} from '@material-ui/core';
+import {Paper, Typography} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
+import Scrollbars from 'react-custom-scrollbars';
 
 const classes = theme => ({
     pokeWrapper: {
@@ -12,6 +13,20 @@ const classes = theme => ({
         margin: 30,
         display: 'flex',
         flexWrap: 'wrap'
+    },
+
+    pokeContent: {
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+
+    title: {
+        margin: theme.spacing(5),
+        marginLeft: theme.spacing(10),
+        letterSpacing: '1.35mm',
+        fontSize: 20,
+        fontWeight: theme.typography.fontWeightLight,
+        textTransform: 'uppercase',
     }
 })
 
@@ -28,12 +43,22 @@ class Pokedex extends React.Component {
         const classes = this.props.classes
         return (
             <div>
-                Lista Pokemonuf
-                <Paper className={classes.pokeWrapper}>
-                    {this.pokemons.map((pokemon) => (
-                        <Pokemon key = {pokemon.id} pokemon={pokemon}/>
-                    ))}
-                </Paper>
+                <Typography className={classes.title}>Pokédex</Typography>
+                <div className={classes.pokeWrapper}>
+                    <Scrollbars
+                        autoHide
+                        autoHideTimeout={1000}
+                        style={{
+                            width: "100%",
+                            height: "75vh",
+                    }} >
+                        <Paper className={classes.pokeContent}>
+                            {this.pokemons.map((pokemon) => (
+                                <Pokemon key = {pokemon.id} pokemon={pokemon}/>
+                            ))}
+                        </Paper>
+                    </Scrollbars>
+                </div>
             </div>
         )
     }
